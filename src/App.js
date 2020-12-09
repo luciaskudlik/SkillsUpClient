7566f6a3fd2ee4c2b1c21b138eecdd538c8ec425
Lucia Skudlik <lucia.skudlik@gmail.com>
2020-12-09 21:19:38 +0100

    add navbar from bootstrap

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import { Switch, Route } from "react-router-dom";
 
-import Navbar from "./components/Navbar";
+import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/Home";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,83 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { withAuth } from "../../context/auth-context";
+
+class Navbar extends Component {
+  render() {
+    // const { user, logout, isLoggedin } = this.props;
+    return (
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <Link to={"/"} className="navbar-brand">
+            SkillsUp
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav">
+              <Link className="nav-link active" aria-current="page" to={"/"}>
+                Home
+              </Link>
+              <Link className="nav-link" to={"/private/profile"}>
+                My Profile
+              </Link>
+
+              {this.props.isLoggedIn ? (
+                <>
+                  <Link onClick={this.props.logout} className="nav-link">
+                    Logout
+                  </Link>
+                  <p>{this.props.user && this.props.user.username}</p>
+                  <img src={this.props.user.img} />
+                </>
+              ) : (
+                <>
+                  <Link className="nav-link" to={"/login"}>
+                    Login
+                  </Link>
+                  <br />
+                  <Link className="nav-link" to={"/signup"}>
+                    Signup
+                  </Link>
+                </>
+              )}
+            </div>
+          </div>
+        </div>
+      </nav>
+
+      // <nav className="navbar">
+      //   <Link to={'/'} id='home-btn'>
+      //     <h4>Home</h4>
+      //   </Link>
+      //   {this.props.isLoggedIn ? (
+      //     <>
+      //       <p>username: {this.props.user && this.props.user.username}</p>
+      //       <button onClick={this.props.logout}>Logout</button>
+      //     </>
+      //   ) : (
+      //     <>
+      //       <Link to="/login">
+      //         <button className="navbar-button">Login</button>{' '}
+      //       </Link>
+      //       <br />
+      //       <Link to="/signup">
+      //         <button className="navbar-button">Sign Up</button>{' '}
+      //       </Link>
+      //     </>
+      //   )}
+      // </nav>
+    );
+  }
+}
+
+export default withAuth(Navbar);
