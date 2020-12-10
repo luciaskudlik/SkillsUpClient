19c68fbe41558ff5f082c4ffee0bf9fa21fc2b62
Lucia Skudlik <lucia.skudlik@gmail.com>
2020-12-10 19:34:54 +0100

    display attended workshops on profile page

--- src/components/WorkshopCard/WorkshopCard.js
+++ src/components/WorkshopCard/WorkshopCard.js
@@ -1,23 +1,24 @@
+import axios from "axios";
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import './WorkshopCard.css';
+import "./WorkshopCard.css";
 
-function WorkshopCard(props) {
-  const workshop = props.workshop;
+class WorkshopCard extends React.Component {
+  render() {
+    const workshop = this.props.workshop;
 
     return (
       <div className="workshop-card">
-        
-          <img className="workshop-card-img" src={workshop.img} alt=""/>
-        
+        <img className="workshop-card-img" src={workshop.img} alt="" />
+
         <h3>{workshop.title}</h3>
         <p>{workshop.credits}</p>
-        <Link to={`/workshops/${workshop._id}`}><button>Learn More</button></Link>
-        
+        <Link to={`/workshops/${workshop._id}`}>
+          <button>Learn More</button>
+        </Link>
       </div>
-      
     );
-  
+  }
 }
 
 export default WorkshopCard;
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,7 +1,30 @@
+import axios from "axios";
 import React, { Component } from "react";
+import WorkshopCard from "../components/WorkshopCard/WorkshopCard";
 import { withAuth } from "../context/auth-context";
 
 class Profile extends Component {
+  state = {
+    attendedWorkshops: [],
+  };
+
+  componentDidMount() {
+    console.log(this.props.user.attendedWorkshops);
+    this.props.user.attendedWorkshops.map((id) => {
+      axios
+        .get(`http://localhost:5000/api/workshops/${id}`)
+        .then((returnedObj) => {
+          this.setState({
+            attendedWorkshops: [
+              ...this.state.attendedWorkshops,
+              returnedObj.data,
+            ],
+          });
+        })
+        .catch((err) => console.log(err));
+    });
+  }
+
   render() {
     return (
       <div>
@@ -9,7 +32,14 @@ class Profile extends Component {
         <h2>Welcome {this.props.user && this.props.user.username}</h2>
         <img src={this.props.user.img} />
         <p>Your Wallet: {this.props.user.wallet} </p>
-
+        <h2>Your upcoming workshops</h2>
+        {this.state.attendedWorkshops.map((workshop) => {
+          return (
+            <div key={workshop._id}>
+              <WorkshopCard workshop={workshop} />
+            </div>
+          );
+        })}
         {/* 
         <h2>Welcome {this.props.user ? this.props.user.username : null }</h2> 
         */}
