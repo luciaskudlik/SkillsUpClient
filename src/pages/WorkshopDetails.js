2abbf1c92bdcac6856ee18bd49d1ce9249332ab5
Lucia Skudlik <lucia.skudlik@gmail.com>
2020-12-10 16:18:31 +0100

    add signup for workshop button

--- src/pages/WorkshopDetails.js
+++ src/pages/WorkshopDetails.js
@@ -1,10 +1,8 @@
 import React from "react";
 import axios from "axios";
 
-
 class WorkshopDetails extends React.Component {
-
-  state={
+  state = {
     title: " ",
     img: " ",
     description: " ",
@@ -14,46 +12,72 @@ class WorkshopDetails extends React.Component {
     credits: 0,
     participants: [],
     host: " ",
-    location: ""
-
-  }
+    location: "",
+  };
 
   getSingleWorkshop = () => {
     const { id } = this.props.match.params;
-    axios.get(`http://localhost:5000/api/workshops/${id}`)
+    axios
+      .get(`http://localhost:5000/api/workshops/${id}`)
       .then((apiResponse) => {
         const theWorkshop = apiResponse.data;
-        const { title, img, description, category, date, length, credits, participants, host, location } = theWorkshop;
-        this.setState({ title, img, description, category, date, length, credits, participants, host, location });
+        const {
+          title,
+          img,
+          description,
+          category,
+          date,
+          length,
+          credits,
+          participants,
+          host,
+          location,
+        } = theWorkshop;
+        this.setState({
+          title,
+          img,
+          description,
+          category,
+          date,
+          length,
+          credits,
+          participants,
+          host,
+          location,
+        });
       })
-      .catch((err) => console.log(err))
-  }
+      .catch((err) => console.log(err));
+  };
 
-  componentDidMount(){
+  componentDidMount() {
     this.getSingleWorkshop();
   }
 
-  render() {
+  handleSubmit = () => {
+    
+  }
 
+
+  render() {
     const date = JSON.stringify(new Date(this.state.date));
 
     return (
       <div>
         <h1>WorkshopDetails Page</h1>
         <h2>{this.state.title}</h2>
-        <img src={this.state.img} alt=""/>
+        <img src={this.state.img} alt="" />
         <p>{this.state.description}</p>
         <p>{date}</p>
         <p>{this.state.length} mins</p>
         <p>price: {this.state.credits} credits</p>
         <p>{this.state.location}</p>
-      
 
+        <button type="submit" onClick={this.handleSubmit}>
+          Sign up for Workshop!
+        </button>
       </div>
     );
   }
- 
 }
 
 export default WorkshopDetails;
-
