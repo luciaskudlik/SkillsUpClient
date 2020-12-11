ca2b4ba72d14ebc32dc558912772bb39f6a97cac
Irene Kyritsis <irene.k@live.ca>
2020-12-11 15:37:02 +0100

    add form component

--- src/components/AddWorkshop/AddWorkshop.js
+++ src/components/AddWorkshop/AddWorkshop.js
@@ -1,13 +1,94 @@
-import React from 'react'
+import React from 'react';
+import axios from 'axios';
+import { withAuth } from "./../../context/auth-context";
 
 class AddWorkshop extends React.Component {
+
+  state = {
+    title: '',
+    description: '',
+    //category: '',
+    date: '',
+    length: 0,
+    credits: 0,
+    maxParticipants: 0,
+    location: ''
+
+  }
+
+  handleInput = (event) => {
+    let { name, value, type } = event.target;
+    console.log(value);
+
+    if (type === 'checkbox') {
+      value = !this.state[name];
+    }
+
+    this.setState({ [name]: value });
+  }
+
+  handleSubmit = (event) => {
+      event.preventDefault();
+      console.log("Buttton submitted")
+      const {title, description, date, length, credits, maxParticipants, location} = this.state;
+      
+      const userId = this.props.user._id;
+      
+      
+      axios
+        .post(
+        "http://localhost:5000/api/workshops",
+        {title, description, date, length, credits, maxParticipants, location, userId},
+        {withCredentials: true})
+        .then((response) => {
+          console.log("sent to DB", response.data);
+          this.props.createWorkshop(response.data);
+        })
+        .catch((err) => console.log(err))
+
+      //clear form here
+
+  }
+
   render() {
     return (
-      <form>
-        <input type="text" placeholder="Workshop"/>
+      <form onSubmit={this.handleSubmit}>
+        <input name ="title" type="text" placeholder="Workshop" value={this.state.title} onChange={this.handleInput}/>
+        <textarea name="description" type="text" placeholder="description" value={this.state.description} onChange={this.handleInput}/>
+        {/* <div>
+          <label for="sports">Beauty</label>
+          <input type="radio" name="category" id="sports" value="Sports" onChange={this.handleInput}/>
+
+          <label for="beauty">Beauty</label>
+          <input type="radio" name="category" id="beauty" value="Beauty" onChange={this.handleInput}/>
+
+          <label for="languages">Beauty</label>
+          <input type="radio" name="category" id="languages" value="Languages" onChange={this.handleInput}/>
+
+          <label for="creativity">Creativity</label>
+          <input type="radio" name="category" id="creativity" value="Creativity" onChange={this.handleInput}/>
+
+          <label for="food-drink">Food & Drink</label>
+          <input type="radio" name="category" id="food-drink" value="Food & Drink" onChange={this.handleInput}/>
+
+          <label for="performing-arts">Performing Arts</label>
+          <input type="radio" name="category" id="performing-arts" value="Performing Arts" onChange={this.handleInput}/>
+
+          <label for="other">Other</label>
+          <input type="radio" name="category" id="other" value="Other" onChange={this.handleInput}/>
+        </div> */}
+
+        <input name="date" type="date" value={this.state.date} onChange={this.handleInput}/>
+        <input name="length" type="number" placeholder="duration" value={this.state.length} onChange={this.handleInput}/>
+        <input name="credits" type="number" placeholder="credits" value={this.state.credits} onChange={this.handleInput}/>
+        <input type="number" name="maxParticipants" placeholder="max number of participants" value={this.state.maxParticipants} onChange={this.handleInput}/>
+        <textarea type="text" name="location" placeholder="location" value={this.state.location} onChange={this.handleInput}/>
+
+        <button type="submit"> Host your workshop </button>
+        
       </form>
     )
   }
 }
 
-export default AddWorkshop;
+export default withAuth(AddWorkshop);
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -7,6 +7,7 @@ import { withAuth } from "../context/auth-context";
 class Profile extends Component {
   state = {
     attendedWorkshops: [],
+    hostedWorkshops: [],
     showForm: false
   };
 
@@ -20,17 +21,32 @@ class Profile extends Component {
         })
         .catch((err) => console.log(err));
     
+
+      axios
+        .get(`http://localhost:5000/api/user`, { withCredentials: true })
+        .then((response) => {
+          this.setState({
+            hostedWorkshops: response.data.hostedWorkshops
+          });
+        })
+        .catch((err) => console.log(err));
 }
 
-  componentDidUpdate(prevProps, prevState) {
-    console.log("Component did update");
-    if (prevState.attendedWorkshops !== this.state.attendedWorkshops) {
-      console.log("YAAAAAAAAY");
-    }
-  }
+  // componentDidUpdate(prevProps, prevState) {
+  //   console.log("Component did update");
+  //   if (prevState.attendedWorkshops !== this.state.attendedWorkshops) {
+  //     console.log("YAAAAAAAAY");
+  //   }
+  // }
 
   toggleForm = () => {
     this.setState({ showForm: !this.state.showForm })
+  }
+
+  addOneWorkshop = (newWorkshop) => {
+    console.log("it works.");
+    const updatedWorkshops = [newWorkshop, ...this.state.hostedWorkshops];
+    this.setState({hostedWorkshops: updatedWorkshops});
   }
 
   render() {
@@ -41,7 +57,17 @@ class Profile extends Component {
         <img src={this.props.user.img} />
         <p>Your Wallet: {this.props.user.wallet} </p>
         <button onClick={this.toggleForm}>Host your own workshop</button>
-        {this.state.showForm ? <AddWorkshop/> : null}
+        {this.state.showForm ? <AddWorkshop createWorkshop={this.addOneWorkshop}/> : null}
+
+        <h2>Your hosted workshops</h2>
+        {this.state.hostedWorkshops.map((workshop) => {
+          return (
+            <div key={workshop._id}>
+              <WorkshopCard workshop={workshop} />
+            </div>
+          );
+        })}
+
         <h2>Your upcoming workshops</h2>
         {this.state.attendedWorkshops.map((workshop) => {
           return (
