bceebf9cc9d3323389abc96ee7e3b1a6becfe0aa
Lucia Skudlik <lucia.skudlik@gmail.com>
2020-12-12 14:40:43 +0100

    add category images

--- src/components/CategoryCard/CategoryCard.js
+++ src/components/CategoryCard/CategoryCard.js
@@ -1,22 +1,15 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import './CategoryCard.css';
-
+import "./CategoryCard.css";
 
 function CategoryCard(props) {
- 
-
-    return (
-       <Link to={`/workshops/category/${props.category}`}>
+  return (
+    <Link to={`/workshops/category/${props.category}`}>
       <div id={props.id} className="categoryCard">
-         <h3>{props.category}</h3>
-
-      
+        <h3>{props.category}</h3>
       </div>
-      </Link>
-      
-    );
-  
+    </Link>
+  );
 }
 
 export default CategoryCard;
