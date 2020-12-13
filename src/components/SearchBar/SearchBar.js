090d569bea62c0ac1fb42a5e2727755747fb4f25
Irene Kyritsis <irene.k@live.ca>
2020-12-13 10:50:23 +0100

    start implementation of searchbar

---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+
+  state ={
+    search: ''
+  }
+
+  handleInput = (event) => {
+    const { value, name } = event.target;
+
+    this.setState({ [name]: (value) })
+    this.props.filterWorkshops(value);
+  }
+
+
+
+  render() {
+
+    return (
+      <div>
+        <input type="text" 
+        className="input search-bar" 
+        name="search" 
+        placeholder="search" 
+        value={this.state.search}
+        onChange={this.handleInput}
+        />
+      </div>
+    )
+  }
+}
+
+export default SearchBar;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,24 +1,35 @@
 import React from 'react';
 import CategoryCard from './../components/CategoryCard/CategoryCard';
 import './../App.css';
+import SearchBar from './../components/SearchBar/SearchBar'
 
-function Home() {
-  return (
-    <div> 
-    <div id="banner">
-      <h1>The key to success is dedication to life-long learning.</h1>
-    </div>
-      
-      <CategoryCard category="Sports" id="sports"/>
-      <CategoryCard category="Beauty" id="beauty"/>
-      <CategoryCard category="Languages" id="languages"/>
-      <CategoryCard category="Creativity" id="creativity"/>
-      <CategoryCard category="Food & Drink" id="food-drink"/>
-      <CategoryCard category="Performing Arts" id="performing-arts"/>
-      <CategoryCard category="Other" id="other"/>
+class Home extends React.Component {
 
-    </div>
-  )
+  filterWorkshops = (input) => {
+    
+  }
+
+  render() {
+    return (
+      <div> 
+      <div id="banner">
+        <h1>The key to success is dedication to life-long learning.</h1>
+      </div>
+
+        <SearchBar filterWorkshops={this.filterWorkshops} />
+  
+        
+        <CategoryCard category="Sports" id="sports"/>
+        <CategoryCard category="Beauty" id="beauty"/>
+        <CategoryCard category="Languages" id="languages"/>
+        <CategoryCard category="Creativity" id="creativity"/>
+        <CategoryCard category="Food & Drink" id="food-drink"/>
+        <CategoryCard category="Performing Arts" id="performing-arts"/>
+        <CategoryCard category="Other" id="other"/>
+
+      </div>
+    )
+  }
 }
 
 export default Home;
